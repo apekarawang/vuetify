3cb4111e66cb3510b1fb382e308b54c6fd605aee
Asosiasi Pengusaha Engineering Karawang <39256442+apekarawang@users.noreply.github.com>
2018-08-18 02:15:30 +0700

    chore: add bahasa indonesia

---
+++ src/locale/id.js
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Item per halaman:',
+    rowsPerPageAll: 'Semua',
+    pageText: '{0}-{1} dari {2}',
+    noResultsText: 'Tidak ada data yang sesuai',
+    nextPage: 'Berikutnya',
+    prevPage: 'Sebelumnya'
+  },
+  dataTable: {
+    rowsPerPageText: 'Baris per halaman:'
+  },
+  noDataText: 'Tidak ada data'
+}
